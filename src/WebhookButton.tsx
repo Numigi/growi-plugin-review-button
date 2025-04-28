76318cc8dbe74951064ac2f8357021bbc2174c82
Lanhild <loanj@naver.com>
2025-04-28 13:14:01 -0400

    init version:

---
+++ src/WebhookButton.tsx
@@ -0,0 +1,172 @@
+import React from 'react';
+
+import { GROWI } from '@goofmint/growi-js';
+import { Properties } from 'hastscript';
+import type { Plugin } from 'unified';
+import { Node } from 'unist';
+import { visit } from 'unist-util-visit';
+
+declare const growiFacade : {
+  react: typeof React,
+};
+
+type Webhook = {
+  feedbackMessage: string;
+}
+
+export const webhookButton = (Tag: React.FunctionComponent<any>): React.FunctionComponent<any> => {
+  return ({ children, ...props }) => {
+    try {
+      const { react } = growiFacade;
+      const { useState } = react;
+      const [feedbackMessage, setFeedbackMessage] = useState<Webhook | null>(null);
+      const [buttonDisabled, setButtonDisabled] = useState(false);
+      const { node } = props;
+      const { type, ...params } = JSON.parse(node.properties.title);
+      const handleClick = () => {
+        setButtonDisabled(true);
+      }
+
+      if (!params.webhookButton) {
+        return (
+          <Tag {...props}>{children}</Tag>
+        );
+      }
+      if (!params.webhookUrl) {
+        return (
+          <>
+            <div className="alert alert-danger" role="alert">
+              <code>
+                webhookUrl
+              </code>
+              <text> is required</text>
+            </div>
+
+          </>
+        );
+      }
+      if (!params.webhookSignature) {
+        return (
+          <>
+            <div className="alert alert-danger" role="alert">
+              <code>
+                webhookSignature
+              </code>
+              <text> is required</text>
+            </div>
+
+          </>
+        );
+      }
+      const growi = new GROWI();
+      const getPageId = async () => {
+        const pageId = window.location.pathname.replace(/\//, '');
+        return pageId;
+      }
+
+      const getPageData = async (pageId: string) => {
+        const page = await growi.page({ pageId: pageId });
+        const pageData = {
+          type: type,
+          id: page.id,
+          title: page.path?.substring(page.path?.lastIndexOf("/") + 1),
+          revision: {
+            id: page.revision?.id,
+            author: {
+              id: page.lastUpdateUser?.id,
+              email: page.lastUpdateUser?.email,
+              name: page.lastUpdateUser?.name,
+            },
+            createdAt: page.revision?.createdAt,
+            body: page.revision?.body,
+          }
+        }
+
+        return pageData;
+      }
+
+      const getWebhookData = async () => {
+        // Disables the button to prevent conflicts.
+        // The client would have to refresh the document to use the button again.
+        handleClick();
+        const pageData = await getPageData(await getPageId());
+        const webhookOptions = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-Webhook-Signature": params.webhookSignature,
+          },
+          body: JSON.stringify(pageData),
+        };
+
+        const url = params.webhookUrl;
+        const response = await fetch(url, webhookOptions);
+        const json = await response.json() as Webhook;
+        console.log(json);
+
+        setFeedbackMessage(json);
+      }
+
+      if (type) {
+        return (
+          <>
+            <button
+              className='btn btn-primary text-white'
+              disabled={buttonDisabled}
+              onClick={getWebhookData}
+              type="submit"
+            >
+              {type}
+            </button>
+            {feedbackMessage && (
+              <div className="alert alert-primary" role="alert">
+                <div dangerouslySetInnerHTML={{ __html: feedbackMessage?.feedbackMessage }} />
+              </div>
+            )}
+          </>
+        )
+      }
+    }
+    catch (err) {
+      // console.error(err);
+    }
+    // Return the original component if an error occurs
+    return (
+      <Tag {...props}>{children}</Tag>
+    );
+  };
+};
+
+interface GrowiNode extends Node {
+  name: string;
+  data: {
+    hProperties?: Properties;
+    hName?: string;
+    hChildren?: Node[] | { type: string, value: string, url?: string }[];
+    [key: string]: any;
+  };
+  type: string;
+  attributes: {[key: string]: string}
+  children: GrowiNode[] | { type: string, value: string, url?: string }[];
+  value: string;
+  title?: string;
+  url?: string;
+}
+
+export const remarkPlugin: Plugin = () => {
+  return (tree: Node) => {
+    return visit(tree, 'containerDirective', (node: Node) => {
+      const n = node as unknown as GrowiNode;
+      if (n.name !== 'webhookButton') return;
+      const data = n.data || (n.data = {});
+      // Render your component
+      const { value } = n.children[0] || { value: ''};
+      data.hName = 'a'; // Tag name
+      data.hChildren = [{ type: 'text', value: value }]; // Children
+      // Set properties
+      data.hProperties = {
+        title: JSON.stringify({ ...n.attributes, ...{ webhookButton: true } }), // Pass to attributes to the component
+      };
+    });
+  };
+};
